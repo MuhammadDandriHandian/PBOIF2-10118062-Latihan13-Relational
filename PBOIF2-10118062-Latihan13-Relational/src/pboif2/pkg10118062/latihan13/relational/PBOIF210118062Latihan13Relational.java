8d1b075a6d517e28ab86b17fc464b4fd48f3f8b7
MuhammadDandriHandian <55798941+MuhammadDandriHandian@users.noreply.github.com>
2019-09-26 14:44:08 +0700

    Add files via upload

---
+++ PBOIF2-10118062-Latihan13-Relational/src/pboif2/pkg10118062/latihan13/relational/PBOIF210118062Latihan13Relational.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2.pkg10118062.latihan13.relational;
+
+/**
+ *
+ * @author 
+ * Nama     : Muhammad Dandri Handian
+ * Kelas    : IF-2
+ * NIM      : 10118062
+ */
+public class PBOIF210118062Latihan13Relational {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int bil1 = 10;
+        int bil2 = 20;
+        System.out.println("a == b = " + (bil1 == bil2));
+        System.out.println("a != b = " + (bil1 != bil2));
+        System.out.println("a > b = " + (bil1 > bil2));
+        System.out.println("a < b = " + (bil1 < bil2));
+        System.out.println("b >= a = " + (bil2 >= bil1));
+        System.out.println("b <= a = " + (bil2 <= bil1));
+    }
+    
+}
